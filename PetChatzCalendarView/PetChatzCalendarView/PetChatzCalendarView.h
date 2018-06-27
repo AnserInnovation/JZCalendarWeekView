6f4790ad679c5448c09190447581f2f7f3686143
DA SONG <songdafocus@icloud.com>
2018-06-27 14:55:33 -0500

    add petchatz own framework

---
+++ PetChatzCalendarView/PetChatzCalendarView/PetChatzCalendarView.h
@@ -0,0 +1,19 @@
+//
+//  PetChatzCalendarView.h
+//  PetChatzCalendarView
+//
+//  Created by DA SONG on 6/26/18.
+//  Copyright © 2018 da. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PetChatzCalendarView.
+FOUNDATION_EXPORT double PetChatzCalendarViewVersionNumber;
+
+//! Project version string for PetChatzCalendarView.
+FOUNDATION_EXPORT const unsigned char PetChatzCalendarViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PetChatzCalendarView/PublicHeader.h>
+
+
---
+++ PetChatzCalendarView/PetChatzCalendarView/ReusableViews/JZColumnHeader.swift
@@ -0,0 +1,61 @@
+//
+//  JZColumnHeader.swift
+//  JZCalendarWeekView
+//
+//  Created by Jeff Zhang on 28/3/18.
+//  Copyright © 2018 Jeff Zhang. All rights reserved.
+//
+
+import UIKit
+
+/// Header for each column (section, day) in collectionView (Supplementary View)
+open class JZColumnHeader: UICollectionReusableView {
+    
+    public var lblDay = UILabel()
+    public var lblWeekday = UILabel()
+    let calendarCurrent = Calendar.current
+    let dateFormatter = DateFormatter()
+    
+    public override init(frame: CGRect) {
+        super.init(frame: .zero)
+        setupUI()
+        backgroundColor = .clear
+    }
+    
+    required public init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupUI() {
+        let stackView = UIStackView(arrangedSubviews: [lblWeekday, lblDay])
+        stackView.axis = .vertical
+        stackView.spacing = 2
+        addSubview(stackView)
+        stackView.setAnchorConstraintsEqualTo(centerXAnchor: centerXAnchor, centerYAnchor: centerYAnchor)
+        lblDay.textAlignment = .center
+        lblWeekday.textAlignment = .center
+        lblDay.font = UIFont.systemFont(ofSize: 2)
+        lblWeekday.font = UIFont.systemFont(ofSize: 12)
+    }
+    
+    public func updateView(date: Date) {
+        var weekday = calendarCurrent.component(.weekday, from: date) - 2
+        
+//        lblDay.text = String(calendarCurrent.component(.day, from: date))
+        if (weekday == -1) {
+            weekday = 6
+        } else if (weekday == 7){
+            weekday = 0
+        }
+        lblWeekday.text = dateFormatter.shortWeekdaySymbols[weekday].uppercased()
+        
+        if date.isToday {
+            lblDay.textColor = JZWeekViewColors.today
+            lblWeekday.textColor = JZWeekViewColors.today
+        } else {
+            lblDay.textColor = JZWeekViewColors.columnHeaderDay
+            lblWeekday.textColor = JZWeekViewColors.columnHeaderDay
+        }
+    }
+    
+}
